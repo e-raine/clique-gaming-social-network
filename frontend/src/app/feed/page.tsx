72bbf66a533fe556ff85e4777bfa18e8b31e8f25
oreoo <nigel.cnee@gmail.com>
2025-07-15 17:20:38 +0800

    Add EditPost and AddPost components

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -1,48 +1,59 @@
-'use client'
-import { FeedCard } from "@/components/feed-card/FeedCard"
+"use client";
+import { FeedCard, FeedCardProps } from "@/components/feed-card/FeedCard";
 
 const Feed = () => {
-  return (
-    <div className="gap-2 flex flex-col">
-			<FeedCard
-				type="grid-images"
-				images={[
-					"/images/mob1.jpg",
-					"/images/mob2.jpg",
-					"/images/mob3.jpg",
-					"/images/mob4.jpg",
-					// "/images/mob5.jpg",
-				]}
-				textContent="Survived the Ocean Monument, and here’s proof!"
-				author="Charles"
-				avatarUrl="/images/charles-avatar.jpg"
-				date={new Date()}
-				game="Minecraft"
-				postTitle="Ocean Raid Highlights"
-			/>
-
-			<FeedCard
-				type="text"
-				textContent="Survived the Ocean Monument, and here’s proof!"
-				author="Charles"
-				avatarUrl="/images/charles-avatar.jpg"
-				date={new Date()}
-				game="Minecraft"
-				postTitle="Ocean Raid Highlights"
-			/>
-
-			<FeedCard
-				type="single-image"
-				images={["/images/mob1.jpg"]}
-				textContent="Survived the Ocean Monument, and here’s proof!"
-				author="Charles"
-				avatarUrl="/images/charles-avatar.jpg"
-				date={new Date()}
-				game="Minecraft"
-				postTitle="Ocean Raid Highlights"
-			/>
+    const FeedCardList: FeedCardProps[] = [
+        {	
+			id: 1233,
+            type: "grid-images",
+            images: [
+                "/images/mob1.jpg",
+                "/images/mob2.jpg",
+                "/images/mob3.jpg",
+                "/images/mob4.jpg",
+                // "/images/mob5.jpg",
+            ],
+            textContent: "Survived the Ocean Monument, and here's proof!",
+            author: "Charles",
+            avatarUrl: "/images/charles-avatar.jpg",
+            date: new Date(),
+            game: "Minecraft",
+            postTitle: "Ocean Raid Highlights"
+        },
+        {	
+			id: 2131232,
+            type: "text",
+            textContent: "Survived the Ocean Monument, and here's proof!",
+            author: "Charles",
+            avatarUrl: "/images/charles-avatar.jpg",
+            date: new Date(),
+            game: "Minecraft",
+            postTitle: "Ocean Raid Highlights"
+        },
+        {
+			id: 32113212,
+            type: "single-image",
+            images: ["/images/mob1.jpg"],
+            textContent: "Survived the Ocean Monument, and here's proof!",
+            author: "Charles",
+            avatarUrl: "/images/charles-avatar.jpg",
+            date: new Date(),
+            game: "Minecraft",
+            postTitle: "Ocean Raid Highlights"
+        }
+    ];
+    
+    return (
+        <div className="gap-2 flex flex-col">
+            {FeedCardList.map((feedCard, index) => (
+                <FeedCard
+                    key={index}
+                    {...feedCard}
+                />
+            ))}
+			
 		</div>
-  )
-}
+    );
+};
 
-export default Feed
+export default Feed;
