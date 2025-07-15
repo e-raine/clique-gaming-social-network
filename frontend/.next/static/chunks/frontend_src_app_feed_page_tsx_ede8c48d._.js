eeb6142c1d0245a66de4c07fee6f3192f2779474
oreoo <nigel.cnee@gmail.com>
2025-07-15 21:12:09 +0800

    Update build artifacts for feed page and favicon route
    Replaces frontend_src_8eb08ab1._.js with frontend_src_f9f33c23._.js in build manifests and updates related chunk references. Adds favicon route to app-paths-manifest and introduces new SSR/server chunk files for updated feed page logic.

--- frontend/.next/static/chunks/frontend_src_app_feed_page_tsx_ede8c48d._.js
+++ frontend/.next/static/chunks/frontend_src_app_feed_page_tsx_ede8c48d._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/frontend_src_8eb08ab1._.js",
+  "static/chunks/frontend_src_f9f33c23._.js",
   "static/chunks/node_modules_efad0079._.js",
   "static/chunks/frontend_src_components_feed-card_commenting-scrollbar_ee853e2e.css"
 ],
