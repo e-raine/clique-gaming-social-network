8cfa2816cef49613a46e1b63c31c6ca44022f7e7
Krarneas <af.saulon@gmail.com>
2025-07-19 00:16:41 +0800

    chore: stop tracking frontend/.next build directory

--- frontend/.next/static/chunks/frontend_src_app_auth_login_page_tsx_ede8c48d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_73e9b6d4._.js",
-  "static/chunks/frontend_src_components_auth_auth_995ced35.css"
-],
-    source: "dynamic"
-});
--- frontend/.next/static/chunks/frontend_src_app_newsfeed_page_tsx_ede8c48d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_ea69acce._.js",
-  "static/chunks/frontend_src_components_newsfeed_newsfeed_5726ce81.css"
-],
-    source: "dynamic"
-});
