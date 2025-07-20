91ecfbb5b4501e1b6ad6176b06e9ebdbc03c001c
Krarneas <af.saulon@gmail.com>
2025-07-20 22:06:28 +0800

    chore: ignore .next files

--- frontend/.next/static/chunks/frontend_src_app_auth_signup_page_tsx_ede8c48d._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e5741484._.js",
-  "static/chunks/frontend_src_fa35d799._.js",
-  "static/chunks/frontend_src_components_auth_auth_995ced35.css"
-],
-    source: "dynamic"
-});
--- frontend/.next/static/chunks/frontend_src_app_feed_page_tsx_ede8c48d._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_src_f9f33c23._.js",
-  "static/chunks/node_modules_efad0079._.js",
-  "static/chunks/frontend_src_components_feed-card_commenting-scrollbar_ee853e2e.css"
-],
-    source: "dynamic"
-});
